14d7b5ff3c25ee85b197a635fe09afe4b4b42fcd
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-01-08 15:41:20 +0700

    Create help.js

---
+++ src/help.js
@@ -0,0 +1,104 @@
+const help = (prefix) => {
+	return `
+-==[ _*ABOUT*_ ]==-
+~> ${prefix}info
+~> ${prefix}blocklist
+~> ${prefix}chatlist
+~> ${prefix}bugreport
+
+-==[ _*MAKER*_ ]==-
+~> ${prefix}sticker
+~> ${prefix}stickergif
+~> ${prefix}toimg
+~> ${prefix}tomp3
+~> ${prefix}bpink
+~> ${prefix}3dtext
+~> ${prefix}quotemaker
+
+-==[ _*MEDIA*_ ]==-
+~> ${prefix}trendtwit
+~> ${prefix}ytsearch
+
+-==[ _*EDUCATION*_ ]==-
+~> ${prefix}artinama
+
+-==[ _*TANYA*_ ]==-
+~> ${prefix}apakah
+~> ${prefix}kapankah
+~> ${prefix}bisakah
+
+-==[ _*DOWNLOADER*_ ]==-
+~> ${prefix}pinterest
+~> ${prefix}ytmp3
+~> ${prefix}ytmp4
+~> ${prefix}joox
+
+-==[ _*MEME*_ ]==-
+~> ${prefix}meme
+~> ${prefix}memeindo
+
+-==[ _*GROUP*_ ]==-
+~> ${prefix}opengc
+~> ${prefix}closegc
+~> ${prefix}promote
+~> ${prefix}demote
+~> ${prefix}hidetag
+~> ${prefix}tagall
+~> ${prefix}tagall2
+~> ${prefix}tagall3
+~> ${prefix}tagall4
+~> ${prefix}tagall5
+~> ${prefix}add
+~> ${prefix}kick
+~> ${prefix}listadmins
+~> ${prefix}linkgroup
+~> ${prefix}leave
+~> ${prefix}ownergroup
+
+-==[ _*SOUND*_ ]==-
+~> ${prefix}tts
+
+-==[ _*MUSIC*_ ]==-
+~> ${prefix}lirik
+~> ${prefix}chord
+
+-==[ _*ISLAM*_ ]==-
+~> ${prefix}quran
+
+-==[ _*STALK*_ ]==-
+~> ${prefix}igstalk
+
+-==[ _*WIBU*_ ]==-
+~> ${prefix}neonime
+~> ${prefix}pokemon
+~> ${prefix}nekonime
+
+-==[ _*FUN*_ ]==-
+~> ${prefix}alay
+~> ${prefix}glitch
+
+-==[ _*INFORMATION*_ ]==-
+~> ${prefix}bahasa
+~> ${prefix}kodenegara
+~> ${prefix}kbbi
+~> ${prefix}covid
+
+-==[ _*OWNER*_ ]==-
+~> ${prefix}setprefix
+~> ${prefix}block
+~> ${prefix}unblock
+~> ${prefix}bc
+~> ${prefix}bcgc
+~> ${prefix}clearall
+
+-==[ _*OTHER*_ ]==-
+~> ${prefix}send
+~> ${prefix}wame
+~> ${prefix}virtex
+~> ${prefix}exe
+~> ${prefix}qrcode
+~> ${prefix}quotes
+~> ${prefix}fml`
+}
+
+exports.help = help
